b5c0c31cfcbfa51adc9888947d39ed592d24d7fb
Mosbah-Mohamed <mosbahmohamed147258369@gmail.com>
2023-01-02 11:44:36 +0200

    install project in web storm

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,49 @@
 <template>
-  <div id="app" class="app">
-
-
-    <b-modal class="app_modal" ref="myModal" centered hide-footer hide-header>
-      <div class="flex-center m-5">
-        <!-- <strong>Loading...</strong> -->
-        <b-spinner type="grow"></b-spinner>
-      </div>
-    </b-modal>
-
-    <router-view></router-view>
-  </div>
+    <div id="app" class="app">
+        <b-modal
+            ref="myModal"
+            centered
+            class="app_modal"
+            hide-footer
+            hide-header
+        >
+            <div class="flex-center m-5">
+                <!-- <strong>Loading...</strong> -->
+                <b-spinner type="grow"></b-spinner>
+            </div>
+        </b-modal>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+    name: "App",
 
-  mounted() {
-    window.scrollTo(0, 0);
-    this.showAlert()
-  },
-
-  methods: {
-    showAlert() {
-      this.$refs['myModal'].show()
+    methods: {
+        showAlert() {
+            this.$refs["myModal"].show();
+        },
+        hideAlert() {
+            this.$refs["myModal"].hide();
+        },
     },
-    hideAlert() {
-      this.$refs['myModal'].hide()
-    }
-  },
-  mounted() {
+    mounted() {
+        window.scrollTo(0, 0);
+        this.showAlert();
 
-    this.showAlert();
-
-    setTimeout(() => {
-      this.hideAlert();
-
-    }, 2000)
-
-  },
-}
+        setTimeout(() => {
+            this.hideAlert();
+        }, 2000);
+    },
+};
 </script>
 
 <style lang="scss" scoped>
 ::v-deep .modal-dialog-centered .modal-content {
-  background: none !important;
-  border: none;
-  color: #FFF;
-  font-size: 30px;
+    background: none !important;
+    border: none;
+    color: #fff;
+    font-size: 30px;
 }
 </style>
